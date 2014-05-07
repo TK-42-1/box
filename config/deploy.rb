5c2981b7c24a756a7e640882b5814fd0f93e319e
TK-42-1 <mister.teekay@gmail.com>
2014-05-07 13:47:48 -0500

    added rails collection gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,6 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute "#{sudo} /etc/init.d/nginx restart"
     end
   end
 
