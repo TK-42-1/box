cb7d45096ebf1c1e19af4b8f8f95f2c8a1dea9fb
TK-42-1 <mister.teekay@gmail.com>
2014-06-20 16:18:17 -0500

    mailer configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Boxapp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'box.tetco.com' }
+  config.action_mailer.default_url_options = { :host => 'boxes.tetco.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
