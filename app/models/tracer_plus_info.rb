f9010301ac74f07b6e9b03b768314476feab384d
TK-42-1 <mister.teekay@gmail.com>
2019-08-04 08:09:31 -0500

    Sync destroy record

--- app/models/tracer_plus_info.rb
+++ app/models/tracer_plus_info.rb
@@ -7,9 +7,26 @@ class TracerPlusInfo < ActiveRecord::Base
 
   def sync!
     box = Box.find(box_code)
-    if box && box.update_attributes(location_code: warehouse_code)
-      return update_attribute(:synchronized, true)
+    return false unless box.present?
+
+    sync_box(box) && update_attribute(:synchronized, true)
+  end
+
+  private
+
+  def sync_box(box)
+    if destroyed_at.present?
+      handle_destroy(box)
+    else
+      handle_scan(box)
     end
-    false
+  end
+
+  def handle_destroy(box)
+    box.update_attributes(destroyed_at: destroyed_at)
+  end
+
+  def handle_scan(box)
+    box.update_attributes(location_code: warehouse_code)
   end
 end
--- spec/models/tracer_plus_info_spec.rb
+++ spec/models/tracer_plus_info_spec.rb
@@ -4,22 +4,34 @@ describe TracerPlusInfo do
   describe '#sync_boxes' do
     let(:box) { FactoryGirl.create(:box) }
     let(:first_scan) { FactoryGirl.create(:tracer_plus_info, :stored, box_code: box.id) }
+    let(:destroy_scan) { FactoryGirl.create(:tracer_plus_info, :destroyed, box_code: box.id) }
 
     before do
       first_scan
+      destroy_scan
       TracerPlusInfo.sync_boxes
       first_scan.reload
+      destroy_scan.reload
       box.reload
     end
 
     context 'first scan' do
-
-      it 'syncs warehouse location code'do
+      it 'syncs warehouse location code' do
         expect(box.location_code).to eq(first_scan.warehouse_code.to_s)
       end
 
       it 'marks record as synchronized' do
         expect(first_scan.synchronized).to eq true
+      end
+    end
+
+    context 'destroy scan' do
+      it 'destroyed date' do
+        expect(box.destroyed_at).to eq(destroy_scan.destroyed_at)
+      end
+
+      it 'marks record as synchronized' do
+        expect(destroy_scan.synchronized).to eq true
       end
     end
   end
