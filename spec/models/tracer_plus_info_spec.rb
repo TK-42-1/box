039696b2630fda59be9d81b480b80002f2d3f68e
TK-42-1 <mister.teekay@gmail.com>
2019-08-04 07:45:42 -0500

    Refactor tracer plus info spec top better prepare for destroy

--- spec/models/tracer_plus_info_spec.rb
+++ spec/models/tracer_plus_info_spec.rb
@@ -4,19 +4,25 @@ describe TracerPlusInfo do
   describe '#sync_boxes' do
     let(:box) { FactoryGirl.create(:box) }
     let(:first_scan) { FactoryGirl.create(:tracer_plus_info, :stored, box_code: box.id) }
+    let(:destroy_scan) { FactoryGirl.create(:tracer_plus_info, :destroyed, box_code: box.id) }
 
-    it 'syncs warehouse location code'do
-      expect {
-        expect {
-          TracerPlusInfo.sync_boxes
-          box.reload
-          first_scan.reload
-        }.to change {
-          box.location_code
-        }.to(first_scan.warehouse_code.to_s)
-      }.to change {
-        first_scan.synchronized
-      }.to(true)
+    before do
+      first_scan
+      TracerPlusInfo.sync_boxes
+      first_scan.reload
+      box.reload
     end
+
+    context 'first scan' do
+
+      it 'syncs warehouse location code'do
+        expect(box.location_code).to eq(first_scan.warehouse_code.to_s)
+      end
+
+      it 'marks record as synchronized' do
+        expect(first_scan.synchronized).to eq true
+      end
+    end
+
   end
 end
