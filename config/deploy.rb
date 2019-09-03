7b7bc95dc3b9bd793db4ac564650fdf16b89e7e8
TK-42-1 <mister.teekay@gmail.com>
2019-09-03 16:23:10 -0500

    Ensure whenever is run when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,17 @@ set :deploy_user, 'deploy'
 set :repo_url, 'git@github.com:TK-42-1/box.git'
 
 set :scm, :git
-set :whenever_roles, [:web, :app]
-set :whenever_command_environment_variables, {}
+set :whenever_roles,        ->{ :db }
+set :whenever_command,      ->{ [:bundle, :exec, :whenever] }
+set :whenever_command_environment_variables, ->{ fetch(:default_env).merge(rails_env: fetch(:whenever_environment)) }
+set :whenever_identifier,   ->{ fetch :application }
+set :whenever_environment,  ->{ fetch :rails_env, fetch(:stage, "production") }
+set :whenever_variables,    ->{ "environment=#{fetch :whenever_environment}" }
+set :whenever_load_file,    ->{ nil }
+set :whenever_update_flags, ->{ "--update-crontab #{fetch :whenever_identifier} --set #{fetch :whenever_variables}" }
+set :whenever_clear_flags,  ->{ "--clear-crontab #{fetch :whenever_identifier}" }
+set :whenever_path,         ->{ release_path }
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -13,7 +13,7 @@
 #   rake "some:great:rake:task"
 # end
 #
-every 10.minutes, roles: :app do
+every 10.minutes do
   runner "TracerPlusInfo.sync_boxes"
 end
 
